c032d491bffd31b4072d16b6e54be28c02e46506
Alex Pezzini <axel.pezzo@gmail.com>
2026-05-05 19:08:14 +0200

    refactor(migration): update superuser migration to create a company and user with proper associations; remove seed data files

--- Api/db/migrations/1688550513962-superuser.js
+++ Api/db/migrations/1688550513962-superuser.js
@@ -1,19 +1,37 @@
 /* eslint-disable no-console */
+const Company = require('../../models/company');
 const User = require('../../models/user');
 require('../connect');
 
-const adminUser = new User({
-  email: 'test@meblabs.com',
-  name: 'Admin',
-  lang: 'IT',
-  active: true,
-  roles: ['superuser']
-});
-
 module.exports.up = async () => {
-  adminUser.password = 'testtest';
+  const company = await new Company({
+    name: 'MebLabs',
+    lang: 'EN',
+    type: 'demo'
+  }).save();
 
-  return adminUser.save();
+  const user = new User({
+    email: 'test@meblabs.com',
+    name: 'Test',
+    lastname: 'User',
+    lang: 'IT',
+    active: true,
+    roles: [],
+    company: {
+      id: company._id,
+      name: company.name,
+      type: company.type,
+      roles: ['admin']
+    }
+  });
+
+  user.password = 'testtest';
+
+  return user.save();
 };
 
-module.exports.down = async () => User.deleteOne({ email: 'test@meblabs.com' });
+module.exports.down = async () => {
+  const user = await User.findOne({ email: 'test@meblabs.com' });
+  if (user?.company?.id) await Company.deleteOne({ _id: user.company.id });
+  return User.deleteOne({ email: 'test@meblabs.com' });
+};
