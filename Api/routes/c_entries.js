363d91aea52995a06c85336eba6e8d71ca3d9839
Alex Pezzini <axel.pezzo@gmail.com>
2026-05-05 17:36:43 +0200

    add controller to entries routes

--- Api/routes/c_entries.js
+++ Api/routes/c_entries.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const controller = require('../controllers/entries');
 const { isAuth } = require('../middlewares/isAuth');
 const rbac = require('../middlewares/rbac');
 const { validator } = require('../middlewares/validator');
@@ -7,13 +8,13 @@ const router = express.Router({ mergeParams: true });
 
 router
   .route('/')
-  .get(isAuth, rbac('entries', 'read'))
-  .post(validator('createEntry'), isAuth, rbac('entries', 'create'));
+  .get(isAuth, rbac('entries', 'read'), controller.getByCompany)
+  .post(validator('createEntry'), isAuth, rbac('entries', 'create'), controller.create);
 
 router
   .route('/:id')
-  .get(validator({ params: 'id' }), isAuth, rbac('entries', 'read'))
-  .patch(validator({ params: 'id', body: 'updateEntry' }), isAuth, rbac('entries', 'update'))
-  .delete(validator({ params: 'id' }), isAuth, rbac('entries', 'delete'));
+  .get(validator({ params: 'id' }), isAuth, rbac('entries', 'read'), controller.getById)
+  .patch(validator({ params: 'id', body: 'updateEntry' }), isAuth, rbac('entries', 'update'), controller.update)
+  .delete(validator({ params: 'id' }), isAuth, rbac('entries', 'delete'), controller.delete);
 
 module.exports = router;
